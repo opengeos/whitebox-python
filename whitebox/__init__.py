6fb73a2cc940cafa97374de6d4390dfabf966690
Qiusheng Wu <giswqs@gmail.com>
2018-11-03 18:34:13 -0400

    Bump version: 0.3.0 → 0.3.1

--- whitebox/__init__.py
+++ whitebox/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Qiusheng Wu"""
 __email__ = 'giswqs@gmail.com'
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 
 from .whitebox_tools import WhiteboxTools
 from .wb_runner import Runner
