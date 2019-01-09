4be63e9d8d1aefcb06a4400e241f166f2d15f740
Qiusheng Wu <giswqs@gmail.com>
2019-01-08 21:53:42 -0500

    Bump version: 0.5.1 → 0.6.0

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/giswqs/whitebox',
-    version='0.5.1',
+    version='0.6.0',
     zip_safe=False,
 )
