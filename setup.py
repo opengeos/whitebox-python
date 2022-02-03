9da1d88789cb9ce0f70971700d0e7a280796c6f0
Qiusheng Wu <giswqs@gmail.com>
2022-02-02 21:36:57 -0500

    Bump version: 2.1.0 → 2.1.1

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/giswqs/whitebox',
-    version='2.1.0',
+    version='2.1.1',
     zip_safe=False,
 )
