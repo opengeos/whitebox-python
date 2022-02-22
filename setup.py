cb0d48a0b31adaa30cd3cc2f052dce7322ebfebe
Qiusheng Wu <giswqs@gmail.com>
2022-02-22 09:21:30 -0500

    Bump version: 2.1.1 → 2.1.2

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/giswqs/whitebox',
-    version='2.1.1',
+    version='2.1.2',
     zip_safe=False,
 )
--- whitebox/__init__.py
+++ whitebox/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Qiusheng Wu"""
 __email__ = 'giswqs@gmail.com'
-__version__ = '2.1.1'
+__version__ = '2.1.2'
 
 from .whitebox import *
 
