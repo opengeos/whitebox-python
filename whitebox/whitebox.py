9a59fab771b183d0015f5ba108d13bdc9d406834
Qiusheng Wu <giswqs@gmail.com>
2023-03-29 13:54:26 -0400

    Import WBT by default

--- whitebox/whitebox.py
+++ whitebox/whitebox.py
@@ -2,9 +2,8 @@
 
 """Main module."""
 
-from .whitebox_tools import download_wbt
+from .whitebox_tools import download_wbt, WhiteboxTools
 
 def Runner(clear_app_state=False, callback=None):
-    from .whitebox_tools import WhiteboxTools
     wbt = WhiteboxTools()
     wbt.launch_wb_runner(clear_app_state, callback)
