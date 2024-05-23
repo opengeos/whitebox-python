f8cc2e65541107e9256a6aa76f0f896512982d24
Qiusheng Wu <giswqs@gmail.com>
2024-05-23 19:25:29 -0400

    Bump version: 2.3.1 → 2.3.2

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/opengeos/whitebox',
-    version='2.3.1',
+    version='2.3.2',
     zip_safe=False,
 )
