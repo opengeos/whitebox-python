2347cfa7687c0afd1a2c57ee4a7c28978af7af41
Qiusheng Wu <giswqs@gmail.com>
2024-05-29 09:12:19 -0400

    Update version to v2.3.4

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/opengeos/whitebox-python",
-    version="2.3.3",
+    version="2.3.4",
     zip_safe=False,
 )
